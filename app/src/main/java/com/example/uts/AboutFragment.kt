75a90256657adc7f6ecc68ffade2a4ec133a1dca
naindrapasha <naindraefrizapasha.tkj@gmail.com>
2026-06-22 19:09:15 +0800

    naindra donee

--- app/src/main/java/com/example/uts/AboutFragment.kt
+++ app/src/main/java/com/example/uts/AboutFragment.kt
@@ -1,18 +1,18 @@
 package com.example.uts
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class AboutFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
